953c18379661334e40cc5ab4746fdac0785a7d8f
barneysspeedshop <barneysspeedshop@users.noreply.github.com>
2025-08-26 04:11:41 +0000

    deploy: aaee5b08f7be1a0b3a20dd0c918470844c8fd06d

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -3,7 +3,7 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"flutter_bootstrap.js": "555203092ed7731b821a0d8b93fb1189",
+const RESOURCES = {"flutter_bootstrap.js": "402cd20c3a985623ce05939b8598ab8f",
 "flutter.js": "888483df48293866f9f41d3d9274a779",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
