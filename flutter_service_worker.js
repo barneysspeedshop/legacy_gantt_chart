b3966d9d499fb73d2dadfaa32b88ea6f1cbf5a01
barneysspeedshop <barneysspeedshop@users.noreply.github.com>
2025-09-10 08:49:42 +0000

    deploy: ea81274e67207197d89399584adaca98659321d6

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -30,7 +30,7 @@ const RESOURCES = {"canvaskit/skwasm.js": "1ef3ea3a0fec4569e5d531da25f34095",
 "assets/AssetManifest.bin.json": "a1fee2517bf598633e2f67fcf3e26c94",
 "assets/AssetManifest.bin": "0b0a3415aad49b6e9bf965ff578614f9",
 "assets/shaders/ink_sparkle.frag": "ecc85a2e95f5e9f53123dcaf8cb9b6ce",
-"flutter_bootstrap.js": "6927eb5858a6b3423838480d8ec08721",
+"flutter_bootstrap.js": "dee123a0bd3814985a5cef0d3a50537c",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "index.html": "c16cf4ad3abac3d87c5140dce0fdc478",
 "/": "c16cf4ad3abac3d87c5140dce0fdc478"};
