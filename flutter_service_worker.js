3026fd2677b3dfc08070d1d102499293cac43089
barneysspeedshop <barneysspeedshop@users.noreply.github.com>
2025-12-16 07:48:05 +0000

    deploy: bb261613a6d63002dedc93b2f4d11a9a08ab2348

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -14,7 +14,7 @@ const RESOURCES = {"flutter.js": "24bc71911b75b5f8135c949e27a2984e",
 "assets/shaders/stretch_effect.frag": "40d68efbbf360632f614c731219e95f0",
 "assets/shaders/ink_sparkle.frag": "ecc85a2e95f5e9f53123dcaf8cb9b6ce",
 "assets/AssetManifest.bin.json": "a1fee2517bf598633e2f67fcf3e26c94",
-"assets/fonts/MaterialIcons-Regular.otf": "26d8a16ffca9712fa639951307934a9e",
+"assets/fonts/MaterialIcons-Regular.otf": "47b188f966fd4c83aea88e94a5189d93",
 "assets/NOTICES": "0a5e2d66cff65471f037e86ea9b9c33c",
 "assets/FontManifest.json": "7b2a36307916a9721811788013e65289",
 "assets/AssetManifest.bin": "0b0a3415aad49b6e9bf965ff578614f9",
@@ -33,9 +33,9 @@ const RESOURCES = {"flutter.js": "24bc71911b75b5f8135c949e27a2984e",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "sqflite_sw.js": "31d56f9d0a4e21949c974394594bb029",
 "sqlite3.wasm": "a4fe605c530a5e5dfd4819303dc75829",
-"flutter_bootstrap.js": "4ae3b130baf3c3125cfb415011acee53",
+"flutter_bootstrap.js": "73e891e159c29603e2aba73f844b3aa8",
 "version.json": "91300a0f2f0705d3165834776406b1db",
-"main.dart.js": "402dfb71db5e9c9873d32282b8665fdc"};
+"main.dart.js": "6aea02239e7afe847f90a681e0720cc7"};
 // The application shell files that are downloaded before a service worker can
 // start.
 const CORE = ["main.dart.js",
