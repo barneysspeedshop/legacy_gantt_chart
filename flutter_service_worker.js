7d424737f3bb2b67c0de1cb4105642634304bb48
barneysspeedshop <barneysspeedshop@users.noreply.github.com>
2026-01-05 10:51:55 +0000

    deploy: f94753fc933be807ec6144edd0b08ecc69b6d795

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -33,7 +33,7 @@ const RESOURCES = {"flutter.js": "24bc71911b75b5f8135c949e27a2984e",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "sqflite_sw.js": "31d56f9d0a4e21949c974394594bb029",
 "sqlite3.wasm": "a4fe605c530a5e5dfd4819303dc75829",
-"flutter_bootstrap.js": "7b9bce9b6a5dad81e1e501205e9e7f9f",
+"flutter_bootstrap.js": "fbc02471fa97f9063db02d81387747bb",
 "coi-serviceworker.js": "3f810cc5c79c013aeef4e79a4df8d501",
 "version.json": "91300a0f2f0705d3165834776406b1db",
 "main.dart.js": "acafbef5aa1393da951f3abb060cba33"};
